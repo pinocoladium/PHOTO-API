f8c6aea3881521db57d27cbba702f24cc9f358ed
pinocoladium <utkonosnadereve@bk.ru>
2023-01-12 19:26:41 +0500

    final edits

--- vk.py
+++ vk.py
@@ -1,15 +1,16 @@
 import requests
 import json
+from progress.bar import IncrementalBar
 
 class Vk:
     url = 'https://api.vk.com/method/'
     def __init__(self, token, version):
         self.params = {'access_token': token, 'v': version}
     def get_photos(self, id, count=5):
+        bar = IncrementalBar('Vk progress', max = 3)
         get_photos_url = self.url + 'photos.get'
         get_photos_params = {'album_id': 'profile', 'owner_id': id, 'extended': 1, 'count': count}
         res = requests.get(get_photos_url, params={**self.params, **get_photos_params}).json()
-        print(res)
         if "error" in res.keys():
             print('Error')
             return
@@ -20,20 +21,28 @@ class Vk:
             print('Photo not found. Profile has no photo')
             return 
         else:
-            print(f'Found {res["response"]["count"]} photos')
-            print('Getting information about a photo....')
+            bar.next()
+            print(f' - Found {res["response"]["count"]} photos')
+            bar.next()
+            print(' - Getting information about a photo....')
         list_photos = []
+        list_likes = []
         for el in (res['response']['items']):
             likes = el['likes']['count']
             s = el['date']
             import time
             date = time.strftime('%d.%m.%y', time.gmtime(s))
-            for el_size in el['sizes']:
-                if el_size['type']==  'w':
-                    size = el_size['type']
-                    url_photos = el_size['url']
-                    list_photos.append({'likes': likes, 'date': date, 'size': size, 'url_photos': url_photos})
-                    print('Photo information received....')
+            size = el['sizes'][-1]['type']
+            url_photos = el['sizes'][-1]['url']
+            if likes not in list_likes:
+                name = likes
+            if likes in list_likes:
+                name = f'{likes} - {date}'
+            list_likes.append(likes)
+            list_photos.append({'names': name, 'likes': likes, 'date': date, 'size': size, 'url_photos': url_photos})
+        bar.next()
+        print(' - Photo information received....')
+            
         return list_photos
 
 
--- yandex.py
+++ yandex.py
@@ -1,5 +1,8 @@
 import requests
 import json
+from progress.bar import IncrementalBar
+
+bar = IncrementalBar('overall progress', max = 3)
 
 class Yandex:
     
@@ -18,22 +21,25 @@ class Yandex:
         response = requests.put(url, headers=self.get_headers(), params=params)
 
     def upload_from_vk(self, list_photos, name_folder='photos vk'):
-            if list_photos == None:
-                print('No photo. Loading is not possible')
-                return
+        bar = IncrementalBar('Yandex Progress', max = 2)
+        if list_photos == None:
+            print('No photo. Loading is not possible')
+            return
+        else:
+            bar.next()
+            print(' - Photo loading in progress....')
+            urn = 'v1/disk/resources/upload/'
+            url = self.url + urn
+            self.create_folder(name_folder)
+            for el in list_photos:
+                url_photos = el['url_photos']
+                file_name = el['names']
+                params = {'path': f'/{name_folder}/{file_name}', 'url': url_photos}
+                response = requests.post(url, headers=self.get_headers(), params=params)
+            if response.status_code == 202:
+                bar.next()
+                print(' - Photo successfully uploaded to Yandex.Disk')
             else:
-                print('Photo loading in progress....')
-                urn = 'v1/disk/resources/upload/'
-                url = self.url + urn
-                self.create_folder(name_folder)
-                for el in list_photos:
-                    url_photos = el['url_photos']
-                    file_name = el['likes']
-                    params = {'path': f'/{name_folder}/{file_name}', 'url': url_photos}
-                    response = requests.post(url, headers=self.get_headers(), params=params)
-                    if response.status_code == 202:
-                        print('Photo successfully uploaded to Yandex.Disk')
-                    else:
-                        print('Photo not uploaded. An error has occurred')
-                return
+                print('Photo not uploaded. An error has occurred')
+            return
 
